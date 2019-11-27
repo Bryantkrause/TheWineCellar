35b9561e25146fc255a3c09a8a6ce5f5fb7adbf5
caseyjames95 <caseypurves@gmail.com>
2019-11-26 21:18:41 -0800

    Login credentials collection
    Added functionality to collect username and password when clicking submit.

--- views/index.jsx
+++ views/index.jsx
@@ -4,22 +4,20 @@ const Layout = require('./Layout')
 module.exports = (props) => {
   return (
     <Layout>
-
       <h1 className="loginSpread">The Wine Cellar</h1>
       <form className="loginSpread">
         <label>
           Username:
           <br/>
-        <input type="text" name="name" />
+        <input id="usernameCatch" type="text" name="name" />
           <br/>
           Password:
           <br/>
-        <input type="text" name="name" />
+        <input id="passwordCatch" type="password" name="name" />
         </label>
         <br/> 
-        <input id="submit" type="submit" value="Submit" />
+        <input id="submitBtn" type="submit" value="Submit" />
       </form>
-
     </Layout>
   )
 }
