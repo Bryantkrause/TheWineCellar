25f31d554d82a83c1ba8403b0e084b06faecb976
Derek Smith <dsmith0789@gmail.com>
2019-11-26 20:19:14 -0800

    cleaned up routes

--- routes/wineRoutes.js
+++ routes/wineRoutes.js
@@ -1,23 +1,11 @@
 const { Wines } = require('../models')
 
 module.exports = app => {
-<<<<<<< HEAD
-  // Puts a new wine in the cellar(wine table)
-  app.post('/', (req, res) => {
-    
-    })
-
   // Assigns wine to user
   app.put('/', (req, res) => {
     
     })
 
-  // Shows all the wine collected in the cellar
-  app.get('/', (req, res) => {
-    res.send('')
-    })
-}
-=======
     // get all wines
   app.get('/wines', (req, res ) => {
     Wines.findAll()
@@ -31,8 +19,4 @@ module.exports = app => {
     Wines.create(req.body)
     .then(() => {res.sendStatus(200)})
     .catch(e => console.log(e))})
-  
   }
-
-  
->>>>>>> e32cea9e0120cd3f2d0dcd4e6209dc6536665973
