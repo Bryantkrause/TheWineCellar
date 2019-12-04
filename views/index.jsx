80c8bedec322dd58774275dd134dbd361e897eb2
caseyjames95 <caseypurves@gmail.com>
2019-12-03 19:36:49 -0800

    Updated Cellar
    Updated celler. added JS for cellar page and changes location to pull javascript files.

--- views/index.jsx
+++ views/index.jsx
@@ -18,6 +18,7 @@ module.exports = (props) => {
         <br/> 
         <input id="submitBtn" type="submit" value="Submit" />
       </form>
+      <script src="./app.js"></script>
     </Layout>
   )
 }
