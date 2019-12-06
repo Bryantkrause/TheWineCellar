79edaa8ecb06d43b3418dc63e617a23b1399d934
rsum1 <ryanjsumpter+github@gmail.com>
2019-12-05 18:25:14 -0800

    login is worky

--- public/app.js
+++ public/app.js
@@ -3,24 +3,27 @@ let pass;
 let person;
 
 const addUser = person => {
-    axios.post('./users', person)
-    .then( ({ data }) => {
-      //console.log(data)
+  axios.post('./users', person)
+    .then(({ data }) => {
       localStorage.setItem('userId', data.id)
       localStorage.setItem('username', data.username)
-      })
+      login(person.username, person.password)
+    })
 }
 
 const login = (user, pass) => {
   axios.get(`/users/${user}/${pass}`)
     .then(response => {
-      console.log(response)
+      // console.log(response)
       if (response.data) {
         console.log('pair matches')
-         window.location.pathname = `/cellar`
+        localStorage.setItem('userId', response.data.id)
+        localStorage.setItem('username', response.data.username)
+        window.location.pathname = `/cellar`
       } else {
         console.log('pair doesnt match')
-        window.location.pathname = `/`
+        // window.location.pathname = `/`
+
       }
     })
 }
@@ -29,11 +32,10 @@ document.getElementById('submitBtn').addEventListener('click', e => {
   e.preventDefault()
   console.log('click')
 
-
-    let person = {
-        username: document.getElementById('usernameCatch').value,
-        password: document.getElementById('passwordCatch').value
-    }
-    addUser(person)
+  let person = {
+    username: document.getElementById('usernameCatch').value,
+    password: document.getElementById('passwordCatch').value
+  }
+  addUser(person)
 })
 
