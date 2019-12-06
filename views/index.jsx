e26314a99d0a33dd40788b876df2298187accc3c
rsum1 <ryanjsumpter+github@gmail.com>
2019-12-05 20:24:45 -0800

    login page looks decent

--- views/index.jsx
+++ views/index.jsx
@@ -4,8 +4,9 @@ const Layout = require('./Layout')
 module.exports = (props) => {
   return (
     <Layout>
-      <h1 className="loginSpread">The Wine Cellar</h1>
-      <form className="loginSpread">
+      <div className='loginBackground'>
+        <h1 className="loginText">The Wine Cellar</h1>
+        {/* <form className="loginSpread">
         <label>
           Username:
           <br/>
@@ -17,7 +18,23 @@ module.exports = (props) => {
         </label>
         <br/> 
         <input id="submitBtn" type="submit" value="Submit" />
-      </form>
+      </form> */}
+
+
+        <div className="row">
+          <form className="col s12 loginSpread">
+            <div className="row center">
+              <div className="input-field col s6 loginField">
+                <input placeholder="Username" id="usernameCatch" type="text" name="name" />
+              </div>
+              <div className="input-field col s6 loginField">
+                <input placeholder="Password" id="passwordCatch" type="password" name="name" />
+              </div>
+              <a className="waves-effect waves-light btn center" id="submitBtn">Submit</a>
+            </div>
+          </form>
+        </div>
+      </div>
       <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
       <script src="./app.js"></script>
     </Layout>
