0b88a9ead21808345eb4b4baf9cf9878ad03a342
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2019-12-07 10:01:13 -0800

    updated wineRoutes for route to get all wines associated to a user to add a feature to also get a type of wine from associated user. Also, added some comments to keep track of ending brackets/braces

--- routes/wineRoutes.js
+++ routes/wineRoutes.js
@@ -2,27 +2,31 @@ const { Wines, Users } = require('../models')
 const { wines } = require('../controllers')
 
 module.exports = app => {
-    // get one type of wine
+  // GET one type of wine
   app.get('/wines/:id/:type', (req, res ) => {
-    Wines.findAll({where: {id: parseInt(req.params.id)}},{where: {type: parseInt(req.params.type)}})
-    .then(wines => {
-      res.json(wines)})
-      .catch(e => console.log(e))
-    })
+    Wines.findAll({
+      where: {
+        userId: parseInt(req.params.id),
+        type: req.params.type
+      } // end where
+    }) // end findAll
+    .then(wines => res.json(wines))
+    .catch(e => console.log(e))
+  })
     
-    // get all wines for user
-    app.get('/wines/:id', (req, res ) => {
-      Wines.findAll({where: {userId: parseInt(req.params.id)}})
-      .then(wines => {
-        res.json(wines)})
-        .catch(e => console.log(e))
-      })
+  // GET all wines for one user
+  app.get('/wines/:id', (req, res ) => {
+    Wines.findAll({where: {userId: parseInt(req.params.id)}})
+    .then(wines => res.json(wines))
+    .catch(e => console.log(e))
+  })
   
-   // Add a wine to the database
+  // Add a wine to the database
   app.post('/wines', (req, res) => {
     Wines.create(req.body)
     .then(() => {res.sendStatus(200)})
-    .catch(e => console.error(e))})
-}
+    .catch(e => console.error(e))
+  })
+} // end module.exports
 
   
